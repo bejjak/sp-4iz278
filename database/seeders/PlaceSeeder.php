13d9b127ceaabd836b7fdedfc845c161619d81bc
Jakub Bejvl <bejj01@vse.cz>
2021-05-17 00:24:33 +0200

    fix seeders imports

--- database/seeders/PlaceSeeder.php
+++ database/seeders/PlaceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Place;
 use Illuminate\Database\Seeder;
 
 class PlaceSeeder extends Seeder {
--- database/seeders/SportSeeder.php
+++ database/seeders/SportSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sport;
 use Illuminate\Database\Seeder;
 
 class SportSeeder extends Seeder {
